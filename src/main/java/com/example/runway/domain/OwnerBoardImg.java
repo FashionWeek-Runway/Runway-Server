71793d3ece76a5e2a62fe0b9d897c660a8ffea01
LimHyunwoo <81962309+imenuuu@users.noreply.github.com>
2023-02-13 23:24:44 +0900

    :zap: : "Entity" 추가

---
+++ src/main/java/com/example/runway/domain/OwnerBoardImg.java
@@ -0,0 +1,35 @@
+package com.example.runway.domain;
+
+import lombok.*;
+import org.hibernate.annotations.DynamicInsert;
+import org.hibernate.annotations.DynamicUpdate;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "OwnerBoardImg")
+@Getter
+@Setter
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+@DynamicUpdate
+@DynamicInsert
+public class OwnerBoardImg extends BaseEntity {
+    @Id
+    @Column(name = "id")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "board_id", nullable = false,insertable=false, updatable=false)
+    private OwnerBoard ownerBoard;
+
+    @Column(name="board_id")
+    private Long boardId;
+
+    @Column(name="img_url")
+    private String imgUrl;
+
+}

f4907fa71ec5448f6a2d80c66bd38f348c9f0abb
임현우 <gusdn8926@naver.com>
2023-02-19 18:25:08 +0900

    :zap: : "사장님 소식" 조회 구현

--- src/main/java/com/example/runway/repository/StoreRepository.java
+++ src/main/java/com/example/runway/repository/StoreRepository.java
@@ -13,11 +13,13 @@ public interface StoreRepository extends JpaRepository<Store, Long> {
 
 
     @Query(value="select S.id 'storeId',S.name'storeName',IF((select exists(select * from Keep where Keep.store_id=S.id and Keep.user_id=:userId)),'true','false')'bookmark'," +
-            " latitude,longitude, C2.category'storeCategory' " +
+            " latitude,longitude, C2.category'storeCategory', " +
+            "   (6371*acos(cos(radians(:latitude))*cos(radians(S.latitude))*cos(radians(S.longitude)" +
+            "   -radians(:longitude))+sin(radians(:latitude))*sin(radians(S.latitude))))as distance " +
             " from Store S join StoreCategory SC on S.id = SC.store_id" +
             " join Category C on SC.category_id = C.id " +
-            " join Category C2 on C2.id=main_category where C.category IN (:categoryList) group by S.id",nativeQuery = true)
-    List<GetMapList> getMapListFilter(@Param("categoryList") List<String> categoryList,@Param("userId") Long userId);
+            " join Category C2 on C2.id=main_category where C.category IN (:categoryList) group by S.id order by distance",nativeQuery = true)
+    List<GetMapList> getMapListFilter(@Param("categoryList") List<String> categoryList, @Param("userId") Long userId, @Param("latitude") double latitude,@Param("longitude") double longitude);
 
 
     interface GetMapList{
@@ -39,25 +41,28 @@ public interface StoreRepository extends JpaRepository<Store, Long> {
 
     @Query(value="select S.id 'storeId',\n" +
             "       S.name'storeName',\n" +
-            "       SI.store_img'storeImg'\n" +
+            "       SI.store_img'storeImg',C.category 'mainCategory'\n" +
             "       ,\n" +
-            "       (select GROUP_CONCAT(C.category SEPARATOR ',')\n" +
-            "        from Category C\n" +
-            "                 join StoreCategory SC on SC.category_id = C.id\n" +
-            "                 join Store S2 on SC.store_id = S2.id\n" +
-            "        where S.id = S2.id\n" +
-            "       )as 'storeCategory'\n" +
+            "       (select GROUP_CONCAT(C2.category SEPARATOR ',')\n" +
+            "        from Category C2\n" +
+            "                 join StoreCategory SC2 on SC2.category_id = C2.id\n" +
+            "                 join Store S2 on SC2.store_id = S2.id \n" +
+            "        where S2.id = S.id \n" +
+            "       )as 'storeCategory',\n" +
+            "   (6371*acos(cos(radians(:latitude))*cos(radians(S.latitude))*cos(radians(S.longitude)" +
+            "   -radians(:longitude))+sin(radians(:latitude))*sin(radians(S.latitude))))as distance " +
             "from Store S join StoreCategory SC on S.id = SC.store_id\n" +
-            "join Category C on SC.category_id = C.id\n" +
-            "join StoreImg SI on S.id = SI.store_id where C.category IN (:categoryList) and sequence=1 and S.status=true group by S.id ",
+            "join Category C on SC.category_id = C.id "  +
+            "join StoreImg SI on S.id = SI.store_id where C.category IN (:categoryList) and sequence=1 and S.status=true group by S.id order by distance ",
             countQuery = "select count(*) from Store S join StoreCategory SC on S.id = SC.store_id \n" +
                     "join Category C on SC.category_id = C.id \n" +
                     "join StoreImg SI on S.id = SI.store_id where C.category IN (:categoryList) and sequence=1 and S.status=true group by S.id",
             nativeQuery = true)
-    Page<StoreInfoList> getStoreInfoFilter(@Param("categoryList") List<String> categoryList, Pageable pageReq);
+    Page<StoreInfoList> getStoreInfoFilter(@Param("categoryList") List<String> categoryList, Pageable pageReq,@Param("latitude") double latitude,@Param("longitude") double longitude);
     interface StoreInfoList {
         Long getStoreId();
         String getStoreImg();
+        String getMainCategory();
         String getStoreCategory();
         String getStoreName();
     }

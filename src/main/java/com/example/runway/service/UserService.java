dd838a7298439aa86db385feb5979f3bd0785407
임현우 <gusdn8926@naver.com>
2023-02-23 22:22:30 +0900

    :recycle: : "로그인" 로직 수정

--- src/main/java/com/example/runway/service/UserService.java
+++ src/main/java/com/example/runway/service/UserService.java
@@ -1,8 +1,11 @@
 package com.example.runway.service;
 
 import com.example.runway.domain.User;
+import com.example.runway.dto.home.HomeReq;
 import com.example.runway.dto.user.UserReq;
 
 public interface UserService {
     void postUserLocation(User user, UserReq.UserLocation userLocation);
+
+    void postUserCategory(Long userId, HomeReq.PostUserCategory postUserCategory);
 }

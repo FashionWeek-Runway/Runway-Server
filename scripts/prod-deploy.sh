2f0ee3e1cc8c152b519b14694370702bab926326
LimHyunwoo <81962309+imenuuu@users.noreply.github.com>
2023-03-09 12:47:12 +0900

    :memo: : prod 서버 배포

---
+++ scripts/prod-deploy.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+BUILD_JAR=$(ls /var/www/prod/build/libs/*.jar)
+JAR_NAME=$(basename $BUILD_JAR)
+echo ">>> build 파일명: $JAR_NAME" >> /var/www/prod/deploy.log
+
+echo ">>> build 파일 복사" >> /var/www/prod/deploy.log
+DEPLOY_PATH=/var/www/prod/build/libs/
+cp $BUILD_JAR $DEPLOY_PATH
+
+echo ">>> 현재 실행중인 애플리케이션 pid 확인" >> /var/www/prod/deploy.log
+CURRENT_PID=$(pgrep -f $JAR_NAME)
+
+echo ">>> $CURRENT_PID"
+
+if [ -z $CURRENT_PID ]
+then
+  echo ">>> 현재 구동중인 애플리케이션이 없으므로 종료하지 않습니다." >> /var/www/prod/deploy.log
+else
+  #kill -15 $CURRENT_PID
+  fuser -k 8000/tcp
+  echo ">>> kill -15 $CURRENT_PID" >> /var/www/prod/deploy.log
+  sleep 5
+fi
+
+source ~/.bashrc
+
+echo ">>>환경변수 주입" >> /var/www/prod/deploy.log
+
+DEPLOY_JAR=$DEPLOY_PATH$JAR_NAME
+echo ">>> DEPLOY_JAR 배포"    >> /var/www/prod/deploy.log
+nohup java -jar "-Dspring.profiles.active=prod" $DEPLOY_JAR >> /var/www/prod/deploy.log 2>/var/www/prod/deploy_err.log &

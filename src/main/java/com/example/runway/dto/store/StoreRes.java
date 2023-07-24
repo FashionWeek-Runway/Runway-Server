605e222bc40e5104f9c2777ff86bdf15f6aa379a
임현우 <gusdn8926@naver.com>
2023-07-24 21:50:39 +0900

    :recycle: : 리뷰 수정중

--- src/main/java/com/example/runway/dto/store/StoreRes.java
+++ src/main/java/com/example/runway/dto/store/StoreRes.java
@@ -149,8 +149,13 @@ public class StoreRes {
     public static class ReviewInquiry{
         @ApiModelProperty(notes="이전 리뷰 ID",required = false,example = "1")
         private Long prevReviewId;
+        @ApiModelProperty(notes="이전 리뷰 이미지")
+        private String prevReviewImgUrl;
         @ApiModelProperty(notes="다음 리뷰 ID",required = false,example = "1")
         private Long nextReviewId;
+        @ApiModelProperty(notes="다음 리뷰 이미지")
+        private String nextReviewImgUrl;
+
     }
 
     @Getter
